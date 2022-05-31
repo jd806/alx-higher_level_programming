fbb36a3b76f08e9ca796199e0e97662b9c5eae3b
Isaac Obella <wizlif.144@gmail.com>
2022-05-31 21:06:57 +0300

    fixes
    Signed-off-by: Isaac Obella <wizlif.144@gmail.com>

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print(''.join([chr(a - 32) if a % 2 != 0 else chr(a)
-      for a in range(122, 96, -1)]), end='')
+print('{}'.format(''.join([chr(a - 32) if a % 2 != 0 else chr(a)
+      for a in range(122, 96, -1)]), end=''))
--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    print(' '.join(['Fizz' if a % 3 == 0 else 'Buzz' if a %
+    print(' '.join(['FizzBuzz' if a % 3 == 0 and a %
+                    5 == 0 else 'Fizz' if a % 3 == 0 else 'Buzz' if a %
                     5 == 0 else str(a) for a in range(1, 101)]), end=' ')
