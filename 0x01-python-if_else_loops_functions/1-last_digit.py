ccfc536e7e2cf3631a37aef8c319ed989e18c2d3
Isaac Obella <wizlif.144@gmail.com>
2022-05-31 22:55:53 +0300

    fix
    Signed-off-by: Isaac Obella <wizlif.144@gmail.com>

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = int(str(number)[-1]) * (-1 if number < 0 else 1)
+last_digit = abs(number) % 10 * (-1 if number < 0 else 1)
 str = f"Last digit of {number:d} is {last_digit:d} and "
 if last_digit == 0:
     str += 'is 0'
