c57bcdeaafefbec09480eb8421e870c7c968c40b
Isaac Obella <wizlif.144@gmail.com>
2022-05-31 20:57:29 +0300

    format
    Signed-off-by: Isaac Obella <wizlif.144@gmail.com>

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print(''.join([chr(ord(a) - 32) if ord(a)
-          in range(97, 123) else a for a in str]))
+    print('{}'.format(''.join([chr(ord(a) - 32) if ord(a)
+          in range(97, 123) else a for a in str])))
