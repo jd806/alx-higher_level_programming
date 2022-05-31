5e3a6094e010b63f8146e38ebb8063fcd0c54118
Isaac Obella <wizlif.144@gmail.com>
2022-05-31 22:20:58 +0300

    fix
    Signed-off-by: Isaac Obella <wizlif.144@gmail.com>

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 print('{}'.format(''.join([chr(a) for a in range(97, 123)
-                         if a not in [101, 113]])), end="")
+                           if a not in [101, 113]])), end="")
