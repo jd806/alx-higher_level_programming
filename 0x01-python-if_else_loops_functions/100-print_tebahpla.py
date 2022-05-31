23facc33bc0fe72809c92bc0ad65ac86472c187f
Isaac Obella <wizlif.144@gmail.com>
2022-05-31 21:00:28 +0300

    fix: Header
    Signed-off-by: Isaac Obella <wizlif.144@gmail.com>

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 print(''.join([chr(a - 32) if a % 2 != 0 else chr(a)
       for a in range(122, 96, -1)]), end='')
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def print_last_digit(number):
     ld = str(number)[-1]
     print(ld, end='')
